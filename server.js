87c26773f65d7a3770442dcc56a84d76dab85242
aysan00 <ayushkumarsandil@gmail.com>
2024-03-24 15:58:52 +0530

    Node and express login pages using atlas

--- server.js
+++ server.js
@@ -1,36 +1,75 @@
-const express = require('express');
-const app = express();
-const port = 3000; // Set your desired port number
+var express = require('express');
+var env = require('dotenv').config()
+var ejs = require('ejs');
+var path = require('path');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// mongoose.connect('mongodb://localhost:27017/EV-Charging', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// }, (err) => {
+//   if (!err) {
+//     console.log('MongoDB Connection Succeeded.');
+//   } else {
+//     console.log('Error in DB connection : ' + err);
+//   }
+// });
+//const mongoose = require("mongoose");
 
-// Handle POST request for user registration
-app.post('/register', (req, res) => {
-  const { username, email, password } = req.body;
-  // Implement registration logic (store user data securely)
-  // For simplicity, I'll use an in-memory array to store users
-  users.push({ username, email, password });
-  res.send('Registration successful!');
+mongoose
+  .connect('mongodb+srv://say12345srt:qIdTBCyRqM2f08lA@ev-charging.wtzim2j.mongodb.net/?retryWrites=true&w=majority&appName=EV-Charging', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Database connected!"))
+  .catch(err => console.log(err));
+ 
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
 });
 
-// Handle POST request for user login
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  // Implement login logic (check username/password validity)
-  // For simplicity, I'll use an in-memory array to simulate user authentication
-  const user = users.find(u => u.username === username && u.password === password);
-  if (user) {
-    res.send('Login successful!');
-  } else {
-    res.status(401).send('Invalid username/email or password');
-  }
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');	
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(__dirname + '/views'));
+
+var index = require('./routes/index');
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:2000`);
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.send(err.message);
 });
 
-// Dummy data to simulate user storage (replace with actual database logic)
-const users = [];
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+  console.log('Server is started on http://127.0.0.1:'+PORT);
+});
